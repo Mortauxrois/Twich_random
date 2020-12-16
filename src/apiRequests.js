5ef85e1ec32f717eb8f3cdc0a869a410a0069d47
sol <solo.software.solutions@gmail.com>
2020-12-16 10:52:25 +0300

    Added api requests level

--- src/apiRequests.js
+++ src/apiRequests.js
@@ -1,1 +1,23 @@
-export const CLIENT_ID = 'xxx'; // Use your client ID
+const CLIENT_ID = 'xxx'; // Use your client ID
+
+
+export function fetchRandomStreamers(offset, n){
+    return fetch(`https://api.twitch.tv/kraken/streams/?language=ru&offset=${offset}&limit=${n}`,  
+            {method: 'get',
+             headers: {
+                        'Accept': 'application/vnd.twitchtv.v5+json', 
+                        'Client-ID': `${CLIENT_ID}`}
+            }
+    )
+}
+
+
+export function fetchRandomGameStreamer(gameType, offset, n){
+    return fetch(`https://api.twitch.tv/kraken/streams/?language=ru&game=${gameType}&offset=${offset}&limit=${n}`,  
+                 {method: 'get',
+                        headers: {
+                                'Accept': 'application/vnd.twitchtv.v5+json', 
+                                'Client-ID': `${CLIENT_ID}`}
+                    }
+                 )
+}
